18d482dde79da1fcc98916b1e3a77e2c79be9b83
darthvade <darthvade@sina.com>
2014-06-29 22:01:03 +0800

    2014-06-29 22:01:01 git-auto-saved

--- Week_12/5-24/newspider/newspider/newspider/spiders/lagouspider.py
+++ Week_12/5-24/newspider/newspider/newspider/spiders/lagouspider.py
@@ -5,12 +5,12 @@ from scrapy.spider import Spider
 from scrapy.http import Request
 from newspider.items import NewspiderItem
 
-jobid = 83637
+jobid = 83661
 
 class NewSpider(Spider):
 	name = 'newspider'
 	allowed_domains = ['lagou.com']
-	start_urls = ['http://www.lagou.com/jobs/83638.html']
+	start_urls = ['http://www.lagou.com/jobs/83662.html']
 
 	def parse(self, response):
 		sel = Selector(response)
